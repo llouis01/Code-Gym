8bf5c5ba180ce985154ca065a9e4c2e8cd208d0a
LL <53797568+llouis01@users.noreply.github.com>
2023-12-02 00:18:33 -0500

    Update trebuchet.py
    completed

--- advent/trebuchet.py
+++ advent/trebuchet.py
@@ -5,20 +5,33 @@ Created on Fri Dec  1 21:54:57 2023
 @author: RoiMinuit
 """
 
-import re
+# puzzle one of advent of code
 
-# open, read, and close
-with open('puzzle1.txt', 'r') as f:
-    lines = f.readlines()
-
-# obtain clean records of numbers
-get = []
-for line in lines:
-    get.append(re.findall('\d*', line))
-    for listx in get:
-        for element in listx:
-            if element == '':
-                listx.remove(element)
-                
-def trebuchet(data):
+def trebuchet(fname):
+    # imports
+    import re
     
+    # open, read, and close
+    with open('puzzle1.txt', 'r') as f:
+        lines = f.readlines()
+
+    # obtain clean records of numbers
+    nums = []
+    clean = []
+    for line in lines:
+        nums.append(re.findall('\d', line))
+        for sublist in nums:
+            for element in sublist:
+                element = int(element) if element.isdigit() else 0
+    
+    # process values for addition
+    for sublist in nums:
+        if len(sublist) == 1:
+            for element in sublist:
+                if int(element) < 10:
+                    clean.append(int(element) * 11)
+        elif len(sublist) > 1:
+                clean.append(int(sublist[0] + sublist[-1]))
+    return sum(clean)
+
+print(f"The calibration value is {trebuchet('puzzle1.txt')}.")
