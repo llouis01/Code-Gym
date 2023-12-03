e1a251bbc88af49e0267e6e9cb5880f915d5f35d
LL <53797568+llouis01@users.noreply.github.com>
2023-12-03 00:31:18 -0500

    Update trebuchet.py
    not working

--- advent/trebuchet.py
+++ advent/trebuchet.py
@@ -6,8 +6,8 @@ Created on Fri Dec  1 21:54:57 2023
 """
 
 ## puzzle one of advent of code
-# imports
 import re
+fname = 'puzzle1.txt'
 
 def trebuchet(fname):
     # open, read, and close
@@ -16,7 +16,6 @@ def trebuchet(fname):
 
     # obtain clean records of numbers
     nums = []
-    clean = []
     for line in lines:
         nums.append(re.findall('\d', line))
         for sublist in nums:
@@ -24,6 +23,7 @@ def trebuchet(fname):
                 element = int(element) if element.isdigit() else 0
     
     # process values for addition
+    clean = []
     for sublist in nums:
         if len(sublist) == 1:
             for element in sublist:
@@ -41,14 +41,31 @@ def trebuchet(fname):
 digits = r"one|two|three|four|five|six|seven|eight|nine|\d+"
 num_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,
             'seven':7, 'eight':8, 'nine':9}
-clean2 = []
-clean3 = []
 
 with open('puzzle1.txt', 'r') as f:
     lines = f.readlines()
-    
+
+# extract lines
+clean2 = []
 for line in lines:
     clean2.append(re.findall(digits, line))
-    for sublist in clean2:
+
+# isolate champ nums
+clean3 = []
+for sublist in clean2:
+    if len(sublist) == 1:
         for element in sublist:
-            element = int(element) if element.isdigit() else num_dict[element]
+                clean3.append(element)
+    elif len(sublist) > 1:
+        clean3.append(sublist[0] + sublist[-1])
+        
+# trim numbers down to two digits
+num_dict[re.findall('[a-z]+', clean3[-1])[0]]
+match = re.search('[a-z]+', clean3[-1])
+
+for sub in clean3:
+    match = re.search('[a-z]+', sub)
+    if match == None:
+        pass
+    else:
+        re.sub(match[0], num_dict[match[0]], sub)
