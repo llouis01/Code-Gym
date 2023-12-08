7b1a0a139b7412206189ce89e176adacb7d93365
LL <53797568+llouis01@users.noreply.github.com>
2023-12-07 21:42:48 -0500

    Update scratchcards.py

--- advent/scratchcards.py
+++ advent/scratchcards.py
@@ -6,26 +6,45 @@ Created on Tue Dec  5 20:58:39 2023
 """
 
 import re
+import pandas as pd
 
 # import text data
 with open("puzzle4.txt", 'r') as f:
     lines = f.readlines()
-    
+
 # extract data
 data = []
-win = {}
-played = {}
 for line in lines:
     x = re.sub('Card +\d+: ', "", line).strip()
     x = re.sub("  ", " ", x)
-    x = re.sub("\|", ",", x)
-    data.append(x.strip())
+    x = re.sub(" ", ", ", x)
+    x = re.sub(",? +\| ?+,", " |", x)
+    x = x.split("|")
+    data.append(x)
 
 clean = []
-for sublist in data:
-    for element in sublist:
-        neat = element.split(" | ")
-        clean.append(neat)
-        # for w, p in neat:
-        #     win.add(neat.split("|")[w])
-        #     played.add(neat.split("|")[p])
+for sublist in range(0, len(data)):
+    for element in range(0, 2):
+        clean.append(re.findall('\d+', data[sublist][element]))
+        
+
+win = []
+play = []
+for lst in clean:
+    if len(lst) < 25:
+        win.append(lst)
+    else:
+        play.append(lst)
+
+data = {'win':win, 'play':play}
+df = pd.DataFrame(data)
+print(df)
+
+win2 = []
+play2 = []
+for listx in df.win:
+    for el in listx:
+        win2.append(int(el))
+for listx in df.play:
+    for el in listx:
+        play2.append(int(el))
