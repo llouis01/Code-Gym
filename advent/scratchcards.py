1cbd545641c5c2ee4d550d0a87ebf32d4482feed
LL <53797568+llouis01@users.noreply.github.com>
2023-12-07 23:39:44 -0500

    Update scratchcards.py
    calculating scores

--- advent/scratchcards.py
+++ advent/scratchcards.py
@@ -10,39 +10,60 @@ import pandas as pd
 fname = "puzzle4.txt"
 
 # import text data
-with open(fname, 'r') as f:
-    lines = f.readlines()
+def scratchcard(fname):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+    
+    # extract data
+    data = []
+    for line in lines:
+        x = re.sub('Card +\d+: ', "", line).strip()
+        x = re.sub("  ", " ", x)
+        x = re.sub(" ", ", ", x)
+        x = re.sub(",? +\| ?+,", " |", x)
+        x = x.split("|")
+        data.append(x)
+    
+    clean = []
+    for sublist in range(0, len(data)):
+        for element in range(0, 2):
+            clean.append(re.findall('\d+', data[sublist][element]))
+    
+    
+    win = []
+    hand = []
+    for lst in clean:
+        x = [int(i) for i in lst if i]
+        if len(x) < 25:
+            win.append(x)
+        else:
+            hand.append(x)
+    return win, hand
 
-# extract data
-data = []
-for line in lines:
-    x = re.sub('Card +\d+: ', "", line).strip()
-    x = re.sub("  ", " ", x)
-    x = re.sub(" ", ", ", x)
-    x = re.sub(",? +\| ?+,", " |", x)
-    x = x.split("|")
-    data.append(x)
 
-clean = []
-for sublist in range(0, len(data)):
-    for element in range(0, 2):
-        clean.append(re.findall('\d+', data[sublist][element]))
+## get scores
+win, hand = scratchcard(fname)
 
-
-win = []
-hand = []
-for lst in clean:
-    x = [int(i) for i in lst if i]
-    if len(x) < 25:
-        win.append(x)
-    else:
-        hand.append(x)
-
+# create df
 data = {'win':win, 'play':hand}
 df = pd.DataFrame(data)
 
 # intersect
 inter = []
-for el in df.win:
-    for il in df.play:
-        inter.append(list(set(el) & set(il)))
+for i in range(0, len(df)):
+    xy = list(set(df.win[i]) & set(df.play[i]))
+    inter.append(xy)
+    
+# append to df
+df['sets'] = inter
+
+# score
+scores = []
+for element in df.sets:
+    if len(element) < 1:
+        x = 0
+    elif len(element) == 1:
+        x = 1
+    else:
+        x = 2 ** len(element)
+    scores.append(x)
