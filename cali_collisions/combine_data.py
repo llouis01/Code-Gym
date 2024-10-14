70fa928cadc951bd2f69ebb97c2cf2820dfe766a
LL <53797568+llouis01@users.noreply.github.com>
2024-10-13 23:37:21 -0400

    much faster
    using pandas, program got locked into loading mode. But with spark, took 2 minutes, but gave wrong number of rows. Need to investigate.

---
+++ cali_collisions/combine_data.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 22:43:06 2024
+
+@author: RoiMinuit
+"""
+
+import os
+import pandas as pd
+
+path = 'C:\\Users\\RoiMinuit\\Desktop\\data\\switrs_raw_csvs'
+
+# find folders
+folders = []
+for folder in os.listdir(path):
+    folder = os.path.join(path, folder)
+    folders.append(folder)
+    
+# collect data from each folder
+all_data = []
+for folder in folders:
+    for f in os.listdir(folder):
+        if f.endswith('CollisionRecords.txt'):
+            all_data.append(os.path.join(folder, f))
+            
+# check shape in each file
+for file in all_data:
+    df = pd.read_csv(file, low_memory=False)
+    print(df.shape)
